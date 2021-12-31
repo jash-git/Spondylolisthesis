7e4cfd73309f49eb9ffdba9f3aa33c0c90270510
jash.liao <jash.liao@gmail.com>
2021-12-31 15:43:33 +0800

    Add 202112311543

---
+++ 456.bat
@@ -0,0 +1,3 @@
+ffmpeg -ss 00:00:10 -t 00:00:28 -i "j01.mp4" -vcodec copy -acodec copy  "j01_01.mp4"
+ffmpeg -ss 00:00:39 -t 00:00:35 -i "j01.mp4" -vcodec copy -acodec copy  "j01_02.mp4"
+ffmpeg -ss 00:01:15 -t 00:01:00 -i "j01.mp4" -vcodec copy -acodec copy  "j01_03.mp4"
